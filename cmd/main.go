1bbf535f1e1b0785dbc6c095088a111146750768
Carlos Sanchez <randomouscrap98@aol.com>
2024-04-16 21:35:23 -0400

    Pagination

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
 	"github.com/randomouscrap98/quickfile"
 
+	"github.com/chi-middleware/proxy"
 	"github.com/dustin/go-humanize"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httprate"
-   "github.com/chi-middleware/proxy"
 	"github.com/pelletier/go-toml/v2"
 )
 
@@ -55,7 +57,7 @@ func initServer(config *quickfile.Config) (chi.Router, *http.Server) {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Timeout(time.Duration(config.Timeout)))
-   r.Use(proxy.ForwardedHeaders())
+	r.Use(proxy.ForwardedHeaders())
 	r.Use(httprate.LimitByIP(config.RateLimitCount, time.Duration(config.RateLimitInterval)))
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", config.Port),
@@ -68,30 +70,48 @@ func initServer(config *quickfile.Config) (chi.Router, *http.Server) {
 	return r, s
 }
 
+// Generate the data used for base template data
+func getBaseTemplateData(config *quickfile.Config, r *http.Request) map[string]any {
+	params := r.URL.Query()
+	data := make(map[string]any)
+	data["account"] = ""
+	page, _ := strconv.Atoi(params.Get("page"))
+	if page <= 0 {
+		page = 1
+	}
+	data["page"] = page
+	data["time"] = time.Now().Format(time.RFC3339)
+	data["defaultexpire"] = time.Duration(config.DefaultExpire)
+	account, err := r.Cookie("account")
+	if err == nil {
+		_, ok := config.Accounts[account.Value]
+		if ok {
+			data["account"] = account.Value
+		}
+	}
+	statistics, err := quickfile.GetFileStatistics("", config)
+	if err != nil {
+		log.Printf("WARN: couldn't get statistics: %s\n", err)
+		data["statistics"] = &quickfile.FileStatistics{}
+	} else {
+		data["statistics"] = statistics
+	}
+	pagecount := int(math.Ceil(float64(statistics.Count) / float64(config.ResultsPerPage)))
+	pagelist := make([]int, pagecount)
+	for i := 0; i < pagecount; i++ {
+		pagelist[i] = i + 1
+	}
+	data["pagecount"] = pagecount
+	data["pagelist"] = pagelist
+	return data
+}
+
 func main() {
 	config := initConfig()
 	r, s := initServer(config)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		data := make(map[string]any)
-		data["account"] = ""
-		data["time"] = time.Now().Format(time.RFC3339)
-		data["defaultexpire"] = time.Duration(config.DefaultExpire)
-		account, err := r.Cookie("account")
-		if err == nil {
-			_, ok := config.Accounts[account.Value]
-			if ok {
-				data["account"] = account.Value
-			}
-		}
-		statistics, err := quickfile.GetFileStatistics("", config)
-		if err != nil {
-			log.Printf("WARN: couldn't get statistics: %s\n", err)
-			data["statistics"] = &quickfile.FileStatistics{}
-		} else {
-			data["statistics"] = statistics
-		}
-
+		data := getBaseTemplateData(config, r)
 		tmpl, err := template.New("index.html").Funcs(template.FuncMap{
 			"Bytes":    humanize.Bytes,
 			"BytesI64": func(n int64) string { return humanize.Bytes(uint64(n)) },
